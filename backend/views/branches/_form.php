b835bd3c515b8d6a668f1ec7d56a8576fad3f520
Panagiotis Efopoulos <panagiotis@tessera.gr>
2023-03-31 16:57:12 +0300

    starting point

---
+++ backend/views/branches/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var backend\models\Branches $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="branches-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'companies_company_id')->textInput() ?>
+
+    <?= $form->field($model, 'branch_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'branch_address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'branch_created_date')->textInput() ?>
+
+    <?= $form->field($model, 'branch_status')->dropDownList([ 'ACTIVE' => 'ACTIVE', 'INACTIVE' => 'INACTIVE', ], ['prompt' => '']) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/companies/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var backend\models\Companies $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="companies-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'company_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'company_email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'company_address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'company_created_date')->textInput() ?>
+
+    <?= $form->field($model, 'company_status')->dropDownList([ 'ACTIVE' => 'ACTIVE', 'INACTIVE' => 'INACTIVE', ], ['prompt' => '']) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
